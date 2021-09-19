a22ce8efed46a2257ac9ce53bd11125fe0dbe8f4
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-09-19 17:47:45 +0400

    Update StateManagement.tsx

--- client/src/StateManagement/StateManagement.tsx
+++ client/src/StateManagement/StateManagement.tsx
@@ -1,18 +1,19 @@
 import React, { useMemo, useReducer, useContext } from "react";
+import { IChildren } from "../interfaces/children.interface";
 import reducer from "./Reducer";
 
 export const MainContext = React.createContext(null);
 
 const initialState = { cryptos: [] };
 
-export function StateManagement(props: any) {
+export function StateManagement({ children }: IChildren) {
   const [state, dispatch] = useReducer(reducer, initialState);
   const contextValue = useMemo(() => {
     return { state, dispatch };
   }, [state, dispatch]);
   return (
     <MainContext.Provider value={contextValue as any}>
-      {props.children}
+      {children}
     </MainContext.Provider>
   );
 }
