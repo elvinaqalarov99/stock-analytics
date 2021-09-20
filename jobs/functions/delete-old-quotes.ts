ea9229e20894d1c507a5234004845c7840ff785a
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-09-21 00:07:31 +0400

    moment js added

--- jobs/functions/delete-old-quotes.ts
+++ jobs/functions/delete-old-quotes.ts
@@ -1,8 +1,10 @@
+import moment from "moment";
 import { Quote } from "../../schemas/quote/quote";
 import Logger from "../../utils/logger";
 
 export default () => {
-  Quote.deleteMany({ created_at: { $lt: new Date().getDate() - 3 } })
+  const _3DaysAgo = moment().subtract(10, "days").toDate();
+  Quote.deleteMany({ created_at: { $lte: _3DaysAgo } })
     .then(() => new Logger().info("All old records were deleted Quote"))
     .catch((err: any) => new Logger().info("delete-old-quotes - " + err));
 };
