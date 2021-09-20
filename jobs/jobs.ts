5bfcc9cf0b715c67e3f496a6d825c731701c784f
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-09-20 11:14:53 +0400

    Update jobs.ts

--- jobs/jobs.ts
+++ jobs/jobs.ts
@@ -2,11 +2,13 @@ import cron from "node-cron";
 import fetchCryptos from "./functions/fetch-cryptos";
 import deleteOldQuotes from "./functions/delete-old-quotes";
 
+// run every 6 minutes
 export const fetchCryptosHandler = () =>
   cron.schedule("*/6 * * * *", () => {
     fetchCryptos();
   });
 
+// run every midnight
 export const deleteOldQuotesHanlder = () =>
   cron.schedule("0 0 0 * *", () => {
     deleteOldQuotes();
