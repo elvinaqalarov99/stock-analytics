3e8859691c56d91dea4e77f88be4ac8634f1a2d6
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-09-20 15:53:44 +0400

    Update delete-old-quotes.ts

--- jobs/functions/delete-old-quotes.ts
+++ jobs/functions/delete-old-quotes.ts
@@ -2,9 +2,7 @@ import { Quote } from "../../schemas/quote/quote";
 import Logger from "../../utils/logger";
 
 export default () => {
-  const _3DaysAgo = new Date().getDate() - 3;
-
-  Quote.deleteMany({ created_at: { $lt: _3DaysAgo } })
+  Quote.deleteMany({ created_at: { $lt: new Date().getDate() - 3 } })
     .then(() => new Logger().info("All old records were deleted Quote"))
     .catch((err: any) =>
       new Logger().info("delete-old-quotes - " + err.message)
